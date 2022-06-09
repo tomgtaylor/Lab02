7d44a1456bc3d90d18aa194949f13c820fda7bdf
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-09 00:51:56 -0700

    added math guessing game.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 let userName = prompt("What is your name?");
 alert("Welcome to my site " + userName + ".");
 alert(userName + ", Good luck answering the below questions.");
-//---------------------------Name Prompt-----------------------------
+
+//---------------------------City-----------------------------
 
 let myCity = prompt("Do I live in Spokane, WA?  y / n ")
     if (myCity == "y") {
@@ -15,7 +16,7 @@ let myCity = prompt("Do I live in Spokane, WA?  y / n ")
     }
         //console.log(userName + " answered " + myCity + " about me living in Spokane.");
 
-//---------------------------Name Prompt-----------------------------
+//---------------------------Education-----------------------------
         
 let myEd = prompt("Did I graduate from WSU? y / n ");
 	if (myEd == "y") {
@@ -27,7 +28,7 @@ let myEd = prompt("Did I graduate from WSU? y / n ");
     }
     	//console.log(userName + " answered" + myName + "about whether I graduated from WSU.");
 
-//---------------------------Name Prompt-----------------------------
+//---------------------------Work-----------------------------
 
 let myWork = prompt("Was I elected President of the USA? y / n ");
 	if (myWork == "y") {
@@ -39,7 +40,7 @@ let myWork = prompt("Was I elected President of the USA? y / n ");
     }
 		//console.log(userName + " answered " + myWork + "about if I was elected US President.");
 
-//---------------------------Name Prompt-----------------------------
+//---------------------------Travel-----------------------------
 
 let myTravel = prompt("Did I ever live in China and Taiwan? y / n ");
 	while (myTravel != "y") {
@@ -49,7 +50,7 @@ let myTravel = prompt("Did I ever live in China and Taiwan? y / n ");
     	alert("Well done " + userName + ". On to the next question.");
     }
         //console.log(userName + " answered " + myTravel + about if I was elected US President.);
-//---------------------------Name Prompt-----------------------------
+//---------------------------Age-----------------------------
 
 let myAge = prompt("Am I 28 years old? y / n ");
 	while (myAge != "n") {
@@ -60,18 +61,32 @@ let myAge = prompt("Am I 28 years old? y / n ");
     }
         //console.log(userName + " answered " + myAge + "regarding that I'm 28.")
 
-        let myAnswers = ["basketball", "baseball", "hockey"]
-
 //console.log(myAnswers[1]);
-//---------------------------Name Prompt-----------------------------
+//-----------------------for loop math---------------------------------
+const numAnswer = 7;
+
+for (let i = 0; i < 4; i++) {
+	let numQuestion = prompt("Guess a number between 1 and 15. You get 4 tries.");
+    if (numAnswer == numQuestion) {
+    	alert("That is correct!");
+    	i = 4;
+    } else if (numQuestion > numAnswer){
+    	alert("Too High!");
+    } else if (numQuestion < numAnswer){
+    	alert("Too Low!");
+    }
+}
+//-----------------------for loop---------------------------------
+
+let myAnswers = ["basketball", "baseball", "hockey"];
+
 for (let number = 0; number < 5; number++){
-	let myQuestions = prompt("What are my favorite sports?");
+	let myQuestions = prompt("What are my favorite sports? 3 right answers are basketball, baseball, or hockey.");
 	if (myAnswers[0] == myQuestions || myAnswers[1] == myQuestions || myAnswers[2] == myQuestions) {
     	alert("Correct.");
         number = 5;
     } else {
-    	alert("Wrong."); 
-        //console.log("testing");        
+    	alert("Wrong. Correct answers were basketball, baseball, or hockey."); 
+        //console.log("testing");
     }
-	
 }
